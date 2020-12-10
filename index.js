c5130858ac75402ed68ab164df4436b695626012
Alexis <alexis.lemarchand@ynov.com>
2020-12-10 15:11:58 +0100

    exo 1

---
+++ index.js
@@ -0,0 +1,13 @@
+const button = document.querySelector('button');
+var images = [];
+button.addEventListener('click', event => {
+  var image = new Image();
+  image.src = "D:/Nouveau dossier/img/img1.jpg";
+  images.push(image);
+  console.log(document.getElementById('grid-container'));
+  console.log(images)
+  for (var i = images.length - 1; i >= 0; i--) {
+  	document.getElementById('grid-container').appendChild(images[i]);
+  }
+  
+});
