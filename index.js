1883c8ed805311fe59a34b189957075093d72762
Alexis <alexis.lemarchand@ynov.com>
2020-12-17 12:49:53 +0100

    manifest

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const button = document.querySelector('button');
 var images = [];
-var i = 1;
+var i = 0;
 let html = ``;
-button.addEventListener('click', event => {
-  getImages();
+button.addEventListener('click', async event => {
+  await getImages();
   html += `<div class="card">
     <div class="header">
-      <img src="./img/img${i}.jpg">
+      <img src="${images[i].link}">
     </div>
     <div class="content">
-      <p>Ceci est une description</p>
+      <p>${images[i].desc}</p>
     </div>
   </div>`;
   document.getElementById('grid-container').innerHTML = html
-  if ( i === 5) {
-    i = 1;
+  if ( i === 4) {
+    i = 0;
   } else {
     i++
   }
 });
 
 async function getImages() {
-  const data = await new Promise(fetch('https://wonderful-haibt-c1c7e8.netlify.app/images.json'));
-  console.log(data);
+  const data = await ((await fetch('https://wonderful-haibt-c1c7e8.netlify.app/images.json')).json());
+  images = data;
 }
